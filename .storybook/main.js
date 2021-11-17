ab72de79d33ea5f7ba535c250a5567ab96b19f56
harshzalavadiya <harsh@strandls.com>
2021-11-17 22:44:20 +0530

    refactor: :recycle: view line styles // pulled

--- .storybook/main.js
+++ .storybook/main.js
@@ -20,6 +20,12 @@ module.exports = {
     reactDocgen: false,
   },
   webpackFinal: async (config) => {
+    config.module.rules.push({
+      type: "javascript/auto",
+      test: /\.mjs$/,
+      include: /node_modules/,
+    });
+
     return {
       ...config,
       module: {
