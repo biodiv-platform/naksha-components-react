1892081d83088aaa3a3b0dc2db4c0a13da906b79
harshzalavadiya <harsh@strandls.com>
2021-11-18 17:48:59 +0530

    refactor: :recycle: added gmaps draw to main

--- packages/naksha-components-react/src/index.tsx
+++ packages/naksha-components-react/src/index.tsx
@@ -1,5 +1,6 @@
 export * from "@ibp/naksha-commons";
 export * from "@ibp/naksha-gmaps-draw";
+export * from "@ibp/naksha-gmaps-view";
 export * from "@ibp/naksha-mapbox-draw";
 export * from "@ibp/naksha-mapbox-list";
 export * from "@ibp/naksha-mapbox-view";
