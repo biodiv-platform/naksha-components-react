ab9dd9ae666ebf289acbb6f46ccb0da51c86626f
harshzalavadiya <harsh@strandls.com>
2022-03-16 21:29:54 +0530

    refactor: :recycle: lerna test

--- packages/naksha-mapbox-list/src/components/infobar/content.tsx
+++ packages/naksha-mapbox-list/src/components/infobar/content.tsx
@@ -24,7 +24,7 @@ export default function InfoBarContent({ onClose }) {
         </div>
         <CloseButton onClick={onClose} />
       </div>
-      <div className={tw`bg-gray-100 p-4 flex-shrink-0`}>
+      <div className={tw`bg-gray-100 p-4 flex-shrink-0 rt`}>
         <LayerSelection hideLabel={true} />
       </div>
       <div className={tw`flex flex-col flex-grow overflow-auto gap-3 p-4`}>
