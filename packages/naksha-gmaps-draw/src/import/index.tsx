6a715b39b6c71ffc99ae6c8639fadf997f7a0751
abrahul <abrahul100@gmail.com>
2023-11-23 09:54:28 +0530

    added input validation for latlong import

--- packages/naksha-gmaps-draw/src/import/index.tsx
+++ packages/naksha-gmaps-draw/src/import/index.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { GMAP_FEATURE_TYPES } from "../static/constants";
 
 export default function NakshaImport({
@@ -7,11 +7,19 @@ export default function NakshaImport({
   ButtonComponent,
 }) {
   const importInputRef = useRef<any>();
+  const [isPointsValid, setIsPointsValid] = useState(true);
+  const [pointsErrorMessage, setPointsErrorMessage] = useState("");
 
   const handleOnAddFeature = () => {
     try {
       const txtLatLng = importInputRef?.current?.value;
       const numLatLng = txtLatLng.split(",").map(Number)?.reverse();
+
+      if (!isValidLatLng(numLatLng)) {
+        setIsPointsValid(false);
+        setPointsErrorMessage("Invalid point format. Please enter valid latitude and longitude.");
+        return;
+      }
 
       addFeature({
         type: GMAP_FEATURE_TYPES.POINT,
@@ -22,17 +30,41 @@ export default function NakshaImport({
         },
         coordinates: numLatLng,
       });
+
+      // Reset error state
+      setIsPointsValid(true);
+      setPointsErrorMessage("");
     } catch (e) {
       console.error(e);
+
+      // Set error state
+      setIsPointsValid(false);
+      setPointsErrorMessage("Invalid point format. Please enter valid latitude and longitude.");
     }
   };
 
+  const isValidLatLng = (latLng) => {
+
+    return (
+      Array.isArray(latLng) &&
+      latLng.length === 2 &&
+      !isNaN(latLng[0]) &&
+      !isNaN(latLng[1]) &&
+      typeof latLng[0] === 'number' &&
+      typeof latLng[1] === 'number'
+    );
+  };
+
   return (
-    <div style={{ display: "flex" }}>
+    <div style={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
       {React.cloneElement(InputComponent, { ref: importInputRef })}
+      {!isPointsValid && (
+        <p style={{ color: "red", marginLeft: "10px" }}>{pointsErrorMessage}</p>
+      )}
       {React.cloneElement(ButtonComponent, {
         onClick: handleOnAddFeature,
         type: "button",
+        marginLeft: "10px",
       })}
     </div>
   );
