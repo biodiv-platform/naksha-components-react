73a2be020d3bd90a8110cf83112275ecbc11b536
abrahul <abrahul100@gmail.com>
2024-01-08 16:23:58 +0530

    added  new props to nakshaimport and geojsonimport

--- packages/naksha-gmaps-draw/src/test/index.tsx
+++ packages/naksha-gmaps-draw/src/test/index.tsx
@@ -1,8 +1,16 @@
 import React, { useRef, useState, useEffect } from "react";
 import { GMAP_FEATURE_TYPES } from "../static/constants";
 import NakshaImport from "../import";
+import GeojsonImport from "../geojson";
 
-export default function TestingImport({ ButtonComponent,ModalComponent,addFeature,InputComponent}) {
+export default function TestingImport({
+  ButtonComponent,
+  ModalComponent,
+  addFeature,
+  InputComponent,
+  ButtonComponentModal,
+  DeteteIcon,
+}) {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const handleModalClose = () => {
@@ -15,19 +23,28 @@ export default function TestingImport({ ButtonComponent,ModalComponent,addFeatur
 
   return (
     <div style={{ display: "flex" }}>
-       {React.cloneElement(ModalComponent, {
+      {React.cloneElement(ModalComponent, {
         isOpen: isModalOpen,
         onClose: handleModalClose,
-        nakshaImport:<NakshaImport 
-        addFeature={addFeature}
-        InputComponent={InputComponent}
-        ButtonComponent={ButtonComponent}/>
+        nakshaImport: (
+          <NakshaImport
+            addFeature={addFeature}
+            InputComponent={InputComponent}
+            ButtonComponent={ButtonComponent}
+          />
+        ),
+        geoJSONImport: (
+          <GeojsonImport
+            addFeature={addFeature}
+            ButtonComponent={ButtonComponent}
+            DeleteIcon={DeteteIcon}
+          />
+        ),
       })}
-     {React.cloneElement(ButtonComponent, {
+      {React.cloneElement(ButtonComponentModal, {
         onClick: handleOpenModal,
         type: "button",
       })}
-
     </div>
   );
 }
